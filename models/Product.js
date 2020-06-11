cd73f23e0da6f8d20e0a095526e92dba4d8e4679
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-06-11 13:37:30 -0500

    define columns

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,31 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // validate that value is a decimal
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // set default value of 10
+      // validate that the value is numeric
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      // reference category model's id
+    }
   },
   {
     sequelize,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,20 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      // reference product model's id
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // reference tag model's id
+    }
   },
   {
     sequelize,
